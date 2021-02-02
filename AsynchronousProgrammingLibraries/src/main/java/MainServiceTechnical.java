78e80d4f5d4078e5d0873f5f07eb224416a45f25
Jorge Daniel Ganem López <jorgeglopez@gmail.com>
2021-02-02 10:58:19 -0500

    Add files via upload

--- AsynchronousProgrammingLibraries/src/main/java/MainServiceTechnical.java
+++ AsynchronousProgrammingLibraries/src/main/java/MainServiceTechnical.java
@@ -8,8 +8,9 @@ import java.util.concurrent.*;
 public class MainServiceTechnical {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        diagnostic();
-        assignAppointment();
+        //diagnostic();
+        //assignAppointment();
+        thenAcceptCompletableFuture();
     }
 
 
@@ -35,6 +36,18 @@ public class MainServiceTechnical {
         String result = Async.await(completableFuture);
 
         System.out.println("Result is "+ result);
+    }
+
+    private static void thenAcceptCompletableFuture() throws ExecutionException, InterruptedException {
+        TechnicalOperations technicalOperations = new TechnicalOperations();
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(()->technicalOperations.ServiceTechnical("Fail"));
+        CompletableFuture<String> completableFutureGreat =
+                CompletableFuture.supplyAsync(()->"Repair Date: "+technicalOperations.assignDateOfTechnical());
+
+        CompletableFuture<Void> future = completableFuture.thenAccept((date)->System.out.println("The number is"+date));
+        CompletableFuture<String> futureGreat = completableFutureGreat.thenApply((message)->message +" Technical is Jorge");
+        //System.out.println(future.get());
+        System.out.println(futureGreat.get());
     }
 
 
